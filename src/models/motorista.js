aebd748da6692a5eae26f7dcc375c933b0c96f15
EderOliEDU <eder.oliveira@edu.rondonopolis.mt.gov.br>
2026-02-12 10:02:07 -0400

    Create motorista model

---
+++ src/models/motorista.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class Motorista {
+    constructor(nome, documento, telefone) {
+        this.nome = nome;
+        this.documento = documento;
+        this.telefone = telefone;
+    }
+}
+
+module.exports = Motorista;
