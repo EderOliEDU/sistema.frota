a7a187e8b735cc4205221ac412ddcf08ef339a27
EderOliEDU <eder.oliveira@edu.rondonopolis.mt.gov.br>
2026-02-12 10:37:18 -0400

    Create veiculo.js model

---
+++ src/models/veiculo.js
@@ -0,0 +1,10 @@
+class Veiculo {
+    constructor(marca, modelo, ano, placa) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.placa = placa;
+    }
+}
+
+module.exports = Veiculo;
